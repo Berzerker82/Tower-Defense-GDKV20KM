4660ffc641d53d23ec78f2544f879ff84373df86
Berzerker82 <annele_kuusrainen@hotmail.com>
2022-10-04 19:50:20 +0300

    Up until animation!

--- Tower Defense GDKV20KM/Assets/Scripts/EnemyController.cs
+++ Tower Defense GDKV20KM/Assets/Scripts/EnemyController.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
-    public Transform target;
+
+    private Path thePath;
+    private int currentPoint;
+    private bool reachedEnd;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        thePath = FindObjectOfType<Path>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if (!reachedEnd)
+        {
+            transform.LookAt(thePath.points[currentPoint]);
+
+            transform.position = Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, thePath.points[currentPoint].position) < .01f)
+            {
+                currentPoint = currentPoint + 1;
+                if (currentPoint >= thePath.points.Length)
+                {
+                    reachedEnd = true;
+                }
+            }
+        }
     }
 }
